93ef689b626c832de68b981eed34b97b21727958
Khaled Abdelhady <khaled.abdelhady@badrit.com>
2012-11-26 08:07:05 +0200

    Removed unnecessary folders and files

--- lib/controllers/action_controller.rb
+++
@@ -1,5 +0,0 @@
-class ActionController::Base
-  def add_category
-    TranslationCenter::Category.create(name: controller_name) unless TranslationCenter::Category.find_by_name(controller_name)
-  end
-end
