7e7844ae6e779c4727e7ca486cbe91cdbdfdd4cb
Mahmoud <mahmoud.khaled@badrit.com>
2012-12-06 12:45:27 +0200

    update readme installation guide

--- lib/translation_center/engine.rb
+++ lib/translation_center/engine.rb
@@ -1,3 +1,14 @@
+require "jquery-rails"
+require 'mysql2'
+require 'haml'
+require 'haml-rails'
+require 'acts_as_votable'
+require 'ya2yaml'
+require 'will_paginate'
+require 'devise'
+require 'meta_search'
+require 'js-routes'
+
 module TranslationCenter
   class Engine < ::Rails::Engine
     isolate_namespace TranslationCenter
