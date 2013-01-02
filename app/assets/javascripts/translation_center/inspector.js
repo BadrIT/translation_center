41a72f05c469827dba7b769c2d7db6499e0d64ba
Khaled Abdelhady <khaled.abdelhady@badrit.com>
2013-01-02 09:36:09 +0200

    red icons for missing keys and green for translated keys

--- app/assets/javascripts/translation_center/inspector.js
+++ app/assets/javascripts/translation_center/inspector.js
@@ -4,17 +4,23 @@ $(document).ready(function() {
   if($('body').children('#inplace_editing').length == 0)
     $('body').append('<div id="inplace_editing"></div>');
     
+  // for each key add a link that goes to the key page
   $('.inplace_key').each(function(){
     var top = $(this).offset().top - 20;
     var left = $(this).offset().left - 10;
     var id = $(this).data('id');
-    $('#inplace_editing').append($('<a>').attr('target', '_blank').attr('href', Routes.translation_center_translation_key_path(id)).attr('data-id', id).attr('style', 'left:' + left + 'px;top:' + top + 'px' ).attr('class', 'icon-edit badge badge-info inplace_edit_button'));
+    // missing translation will be in red while translated will be in green
+    var badgeClass = $(this).hasClass('translation_missing') ? 'badge-important' : 'badge-success'
+    $('#inplace_editing').append($('<a>').attr('target', '_blank').attr('href', Routes.translation_center_translation_key_path(id)).attr('data-id', id).attr('style', 'left:' + left + 'px;top:' + top + 'px' ).attr('class', 'icon-edit badge ' + badgeClass  + ' inplace_edit_button'));
 
   });
 
+  // highlight the key when user hovers over it
   $(".inplace_edit_button").mouseover(function() {
     var id = $(this).data('id');
-    $('.inplace_key[data-id=' + id + ']').css('color', 'red');
+    var key = $('.inplace_key[data-id=' + id + ']')
+    var color = key.hasClass('translation_missing') ? 'red' : 'green'
+    key.css('color', color);
   }).mouseout(function(){
     var id = $(this).data('id');
     $('.inplace_key[data-id=' + id + ']').css('color', '');
