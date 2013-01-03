c6680995edd65d941942ca82517ec59fc01d3bf7
Khaled Abdelhady <khaled.abdelhady@badrit.com>
2013-01-03 17:03:38 +0200

    minors in view of changes

--- app/helpers/translation_center/center_helper.rb
+++ app/helpers/translation_center/center_helper.rb
@@ -5,10 +5,10 @@ module TranslationCenter
     def format_change(attribute, value)
       # if value is an array then changed value
       if value.is_a? Array 
-        "#{attribute} changed from '#{value[0]}' to '#{value[1]}'"
+        "<span class=\"label label-info\">#{attribute}</span> changed from <span class=\"label label-important\">#{value[0]}</span> to <span class=\"label label-success\">#{value[1]}</span>".html_safe
       # else value is new
       else
-        "#{attribute} has new value '#{value}'"
+        "<span class=\"label label-info\">#{attribute}</span> has new value <span class=\"label label-success\">#{value}</span>".html_safe
       end
     end
   end
