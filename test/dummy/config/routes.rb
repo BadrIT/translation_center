8090a72d30113d342a0d38c4681779fa3c997b6b
Khaled Abdelhady <khaled.abdelhady@badrit.com>
2013-04-10 10:07:50 +0200

    clean dummy application

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-
-  resources :posts
-
-
   mount TranslationCenter::Engine => "/translation_center"
-  root to: 'posts#index'
 end
--- test/dummy/db/migrate/20121219092928182074_create_translation_center_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateTranslationCenterCategories < ActiveRecord::Migration
-  def change
-    create_table :translation_center_categories do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
