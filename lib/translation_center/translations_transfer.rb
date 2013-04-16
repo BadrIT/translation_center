4d661e0de8ec454b42b49c6d81e36b48c0191973
Khaled <khaled.hady.mail@gmail.com>
2013-04-16 15:58:53 +0200

    split yaml2db to methods to make the code less complex

--- lib/translation_center/translations_transfer.rb
+++ lib/translation_center/translations_transfer.rb
@@ -12,19 +12,68 @@ module TranslationCenter
   end
 
   def self.collect_keys(scope, translations)
-      full_keys = []
-      translations.to_a.each do |key, translations|
-        new_scope = scope.dup << key
-        if translations.is_a?(Hash)
-          full_keys += collect_keys(new_scope, translations)
-        else
-          full_keys << new_scope.join('.')
-        end
+    full_keys = []
+    translations.to_a.each do |key, translations|
+      new_scope = scope.dup << key
+      if translations.is_a?(Hash)
+        full_keys += collect_keys(new_scope, translations)
+      else
+        full_keys << new_scope.join('.')
       end
-      return full_keys
+    end
+    return full_keys
+  end
+
+  # gets the translation of a a key in certian lang and inserts it in the db
+  # returns true if the translation was fonud in yaml 
+  def self.yaml2db_key(locale, translation_key, translator, all_yamls)
+    I18n.locale = locale
+    translation = TranslationCenter::Translation.find_or_initialize_by_translation_key_id_and_lang_and_user_id(translation_key.id, locale.to_s, translator.id)
+    
+    # get the translation for this key from the yamls
+    value = get_translation_from_hash(translation_key.name, all_yamls[locale])
+    unless value.blank?
+      translation.update_attribute(:value, value)
+      # accept this yaml translation
+      translation.accept if TranslationCenter::CONFIG['yaml2db_translations_accepted']
+      true
+    end
+  end
+
+  # takes array of keys and creates/updates the keys in the db with translations in the given locales
+  def self.yaml2db_keys(keys, translator, locales, all_yamls)
+    # initialize stats variables
+    new_keys = 0
+    missing_keys = locales.inject({}) do |memo, lang|
+      memo[lang] = 0
+      memo
     end
 
-  def self.yaml2db(locale)
+    # for each key create it in the db if it doesn't exist, and add its translation to
+    # the db in every locale
+    keys.each do |key|
+
+      translation_key = TranslationCenter::TranslationKey.find_or_initialize_by_name(key)
+      if translation_key.new_record?
+        translation_key.save
+        new_keys += 1
+      end
+
+      # for each locale create/update its translation
+      locales.each do |locale|
+        missing_keys[locale] += 1 unless self.yaml2db_key(locale, translation_key, translator, all_yamls)
+      end
+
+    end
+
+    puts "found new #{new_keys} key(s)"
+    missing_keys.each do |locale, count|
+      puts "missing #{count} translation(s) for #{locale}" if count > 0
+    end
+  end
+
+  # take the yaml translations and update the db with them
+  def self.yaml2db(locale=nil)
 
     # prepare translator by creating the translator if he doesn't exist
     translator = TranslationCenter.prepare_translator
@@ -45,47 +94,16 @@ module TranslationCenter
     all_keys = all_yamls.collect do |check_locale, translations|
       collect_keys([], translations).sort
     end.flatten.uniq
+
     puts "#{all_keys.size} #{all_keys.size == 1 ? 'unique key' : 'unique keys'} found."
 
     locales = locale.blank? ? I18n.available_locales : [locale.to_sym]
 
-    new_keys = 0
-    missing_keys = locales.inject({}) do |memo, lang|
-      memo[lang] = 0
-      memo
-    end
-
-    all_keys.each do |key|
-      translation_key = TranslationCenter::TranslationKey.find_or_initialize_by_name(key)
-      if translation_key.new_record?
-        translation_key.save
-        new_keys += 1
-      end
-
-      locales.each do |locale|
-        I18n.locale = locale
-        translation = TranslationCenter::Translation.find_or_initialize_by_translation_key_id_and_lang_and_user_id(translation_key.id, locale.to_s, translator.id)
-        # no_default option to prevent the default translation from being created
-        # value = I18n.translate(key, raise: true, yaml: true, no_default: true)
-        value = get_translation_from_hash(translation_key.name, all_yamls[locale])
-        unless value.blank?
-          translation.update_attribute(:value, value)
-          # accept this yaml translation
-          translation.accept if TranslationCenter::CONFIG['yaml2db_translations_accepted']
-        else
-          missing_keys[locale] += 1
-        end
-
-      end
-    end
-
-    puts "found new #{new_keys} key(s)"
-    missing_keys.each do |locale, count|
-      puts "missing #{count} translation(s) for #{locale}" if count > 0
-    end
+    # create records for all keys that exist in the yaml
+    yaml2db_keys(all_keys, translator, locales, all_yamls)
   end
 
-  def self.db2yaml(locale)
+  def self.db2yaml(locale=nil)
     locales = locale.blank? ? I18n.available_locales : [locale.to_sym]
 
     # for each locale build a hash for the translations and write to file
