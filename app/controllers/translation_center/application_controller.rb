181dc33b781574249dcf6de8da70be2a30ed2cdf
Khaled Abdelhady <khaled.abdelhady@badrit.com>
2013-06-25 11:07:11 +0200

    use locale in inspector link to switch locales

--- app/controllers/translation_center/application_controller.rb
+++ app/controllers/translation_center/application_controller.rb
@@ -19,7 +19,7 @@ module TranslationCenter
     def translation_langs_filters
       session[:current_filter] ||= 'untranslated'
       session[:lang_from] ||= :en
-      session[:lang_to] ||= :en
+      session[:lang_to] = params[:lang_to] || session[:lang_to] || :en
       I18n.locale = session[:lang_from] || I18n.default_locale
     end
 
--- lib/translation_center/translation_helpers.rb
+++ lib/translation_center/translation_helpers.rb
@@ -27,7 +27,7 @@ module TranslationCenter
   def wrap_span(translation, translation_key)
     # put the inspector class if inspector is all and the key doesn't belongs to translation_center
     if TranslationCenter::CONFIG['inspector'] == 'all' && translation_key.name.to_s.split('.').first != 'translation_center'
-      "<span class='tc-inspector-key' data-type='#{translation_key.status(I18n.locale)}' data-id='#{translation_key.id}'> #{translation} </span>".html_safe
+      "<span class='tc-inspector-key' data-locale='#{I18n.locale}' data-type='#{translation_key.status(I18n.locale)}' data-id='#{translation_key.id}'> #{translation} </span>".html_safe
     else
       translation
     end
@@ -116,7 +116,7 @@ module I18n
         if TranslationCenter::CONFIG['inspector'] == 'off' || category == 'translation_center'
           %(<span class="translation_missing" title="translation missing: #{keys.join('.')}">#{key}</span>)
         else
-          %(<span class="translation_missing tc-inspector-key" data-type="#{translation_key.status(I18n.locale)}" data-id="#{translation_key.id}" title="translation missing: #{keys.join('.')}">#{key}</span>)
+          %(<span class="translation_missing tc-inspector-key" data-locale='#{I18n.locale}' data-type="#{translation_key.status(I18n.locale)}" data-id="#{translation_key.id}" title="translation missing: #{keys.join('.')}">#{key}</span>)
         end
       end
 
