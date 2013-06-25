b1ee7fb6042eee38572314c048dd61493f434fb2
Khaled Abdelhady <khaled.abdelhady@badrit.com>
2013-06-25 09:54:14 +0200

    fixed missing return causing routes failer when editing a key

--- app/assets/javascripts/translation_center/routes.js
+++ app/assets/javascripts/translation_center/routes.js
@@ -4,7 +4,7 @@ Routes = {
   translation_center_set_lang_from_path: translation_center_root + 'set_language_from/',
   translation_center_search_activity_path: translation_center_root + 'search_activity',
   translation_center_search_translation_keys_path: translation_center_root + '/translation_keys/search',
-  translation_center_translation_key_path: function(id) { translation_center_root + 'translation_keys/' + id },
+  translation_center_translation_key_path: function(id) { return translation_center_root + 'translation_keys/' + id },
   translation_center_category_more_keys_path: function(id) { return translation_center_root + 'categories/' + id + '/more_keys' },
   translation_center_translation_accept_path: function(id) { return translation_center_root + 'translations/' + id + '/accept' },
   translation_center_translation_unaccept_path: function(id) { return translation_center_root + 'translations/' + id + '/unaccept' },
