f5a6ce3eb42e743191729d7697d03406cf108c9c
Mohamed Magdy <mohamed.magdy@sumup.com>
2014-05-04 23:01:55 +0200

    #19 Downgrading Rails version and fixing user model in dummy app

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -8,4 +8,6 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  acts_as_translator
 end
